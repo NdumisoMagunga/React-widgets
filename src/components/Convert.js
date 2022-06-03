53f65006bb3c4f44a08d7ef3771c9b7c7446647f
NdumisoMagunga <magunga55@gmail.com>
2022-06-03 22:39:05 +0200

    convert component complete, added timer for request sending

---
+++ src/components/Convert.js
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const Convert = ({language, text}) => {
+    const [translated, setTranslated] = useState('');
+    const [debouncedtext, setDebouncedText] = useState(text);
+
+    useEffect(() => {
+    const timerId = setTimeout(() => {
+        setDebouncedText(text);
+    }, 500);
+
+    return () => { // timer cleanUP function
+        clearTimeout(timerId);
+    }
+
+    },[text]);
+
+    useEffect(() => {
+        const doTranslation = async () => {
+       const {data} = await axios.post('https://translation.googleapis.com/language/translate/v2',
+            {},
+            {
+                params: {
+                    q:debouncedtext,
+                    target: language.value,
+                    key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+                }
+            });
+            setTranslated(data.data.translations[0].translatedText);
+        }
+       
+        doTranslation();
+
+    }, [language, debouncedtext]);
+
+
+    return (
+        <div>
+            <h1 className='ui header'>{translated}</h1>
+        </div>
+    )
+};
+
+export default Convert;
